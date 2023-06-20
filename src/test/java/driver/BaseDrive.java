7bc21168e50c2978e96fb29473384d6aefaa5bd1
sakib-75 <sakibulislam285@gmail.com>
2023-06-20 14:04:16 +0600

    project re-architecture for mobile automation

--- src/test/java/driver/BaseDrive.java
+++ src/test/java/driver/BaseDrive.java
@@ -1,79 +1,46 @@
 package driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Properties;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
-import static utils.PropertiesDataParser.loadProperties;
 
 public class BaseDrive {
 
-    public static WebDriver driver;
+    public static AppiumDriver driver = null;
 
     @BeforeSuite
-    public void setup(){
-        Properties properties = loadProperties("config.properties");
-        String browser = properties.getProperty("browser");
-        boolean headless_browser = properties.getProperty("headless-browser").equals("true");
+    public static void setup() throws MalformedURLException, URISyntaxException {
+        DesiredCapabilities capability = new DesiredCapabilities();
+        capability.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capability.setCapability(MobileCapabilityType.UDID, "emulator-5554");
 
-        ArrayList<String> headless_options = new ArrayList<>();
-        headless_options.add("--headless");
-        headless_options.add("--window-size=1920, 1080");
+        File appDir = new File("src/test/java/resources/apk");
+        File app = new File(appDir, "prothom_alo.apk");
+        capability.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+        capability.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
 
-        switch (browser){
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--remote-allow-origins=*");
-                if (headless_browser){
-                    chromeOptions.addArguments(headless_options);
-                }
-                driver = new ChromeDriver(chromeOptions);
-                break;
+        String serverUrl = "http://127.0.0.1:4723/wd/hub";
+        URL url = new URI(serverUrl).toURL();
 
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.addArguments("--remote-allow-origins=*");
-                if (headless_browser){
-                    firefoxOptions.addArguments(headless_options);
-                }
-                driver = new FirefoxDriver(firefoxOptions);
-                break;
-
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--remote-allow-origins=*");
-                if (headless_browser){
-                    edgeOptions.addArguments(headless_options);
-                }
-                driver = new EdgeDriver(edgeOptions);
-                break;
-
-            default:
-                throw new RuntimeException(browser + "not found");
-        }
-
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver = new AndroidDriver(url, capability);
 
     }
 
     @AfterSuite
-    public void quit(){
-        driver.quit();
+    public static void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
 }
--- src/test/java/test_cases/HomeTest.java
+++ src/test/java/test_cases/HomeTest.java
@@ -2,22 +2,12 @@ package test_cases;
 
 import driver.BaseDrive;
 import org.testng.annotations.Test;
-import pages.HomePage;
-
-import java.util.Properties;
-
-import static utils.PropertiesDataParser.loadProperties;
 
 public class HomeTest extends BaseDrive {
 
     @Test
-    public void tc1() {
-        Properties properties = loadProperties("config.properties");
-        String url = properties.getProperty("base-url");
-
-        driver.get(url);
-
+    public void homePage() {
+        System.out.println("Home page opened");
     }
-
 
 }
